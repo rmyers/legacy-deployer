42d412ecb619956f8d400098cbcfadb830335bbe
Robert Myers <robert@keyingredient.com>
2011-11-01 19:26:53 -0500

    adding write_file util and removing old unused ones.

--- cannula/utils.py
+++ cannula/utils.py
@@ -17,6 +17,7 @@ except ImportError:  # Compatibility for Python <= 2.6
 
 from django.forms.util import ValidationError
 from django.contrib.admin.helpers import AdminForm
+from django.template.loader import render_to_string
 
 log = getLogger('cannula.utils')
 
@@ -50,11 +51,6 @@ def shell_escape(text):
     text = text.replace('"', '\\"')
     return '"%s"' % text
 
-def format_exception(exc_info=None):
-    if not exc_info:
-        exc_info = sys.exc_info()
-    return '\n'.join(traceback.format_exception(*exc_info))
-
 def import_object(dotted_path):
     """
     Import an object from a dotted path string.
@@ -80,50 +76,14 @@ def add_blank_choice(choices, force=False):
         choices = [("", "---------")] + choices
     return choices
 
-pkg_err = """\
-Must be at least two characters long, be all lowercase, and start with an
-alpha character.  Numbers are allowed anywhere except the first
-character. Underscores are allowed anywhere except the first and last
-characters, but are discouraged.  See the "Naming Conventions" section
-of <a href="http://www.python.org/dev/peps/pep-0008/">PEP 8</a> for
-more info."""
-
-def validate_python_package_name(name, check_conflict=False):
-    """
-    A function to be used in form clean methods that checks if the passed name
-    is a valid Python package name and conforms to Python's naming standards.
-    Returns None if name is valid, and raises a ValidationError otherwise.
-
-    If group is True, then check that the name does not conflict with a
-    built-in module.
-    """
-    if len(name) < 2:
-        raise ValidationError(pkg_err)
-    if not re.match('^[a-z]', name):
-        raise ValidationError(pkg_err)
-    if not re.match('[a-z][a-z0-9_]*[a-z0-9]$', name):
-        raise ValidationError(pkg_err)
-    if check_conflict:
-        try:
-            import_object(name)
-            raise ValidationError("Name conflicts with built-in module")
-        except ImportError:
-            pass
-
-def make_admin_form(form):
-    """
-    Make a regular form an AdminForm, then we can use admin styles.
-    Because form.as_* sucks and looks crappy!
-    """
-    if not hasattr(form, 'fieldsets'):
-        form.fieldsets = (('', {'fields': [f for f in form.fields]}),)
-    if not hasattr(form, 'prepopulated_fields'):
-        form.prepopulated_fields = {}
-    return AdminForm(form, form.fieldsets, form.prepopulated_fields)
-
-
-def make_project(name):
-    """
-    Make the project on the file system.
-    """
+def write_file(file_name, template, context=None):
+    if context is None:
+        context = {}
+    
+    log.info("Writing file: %s", file_name)
+    with open(file_name, 'w') as f:
+        temp = render_to_string(template, context)
+        f.write(temp)
+        f.close()
+    
     
