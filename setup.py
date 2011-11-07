8866c1d2947b41d4433454c27f9161e1348a2de6
Robert Myers <robert@keyingredient.com>
2011-11-07 08:38:37 -0600

    Updating setup file to include console script

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-from distribute_setup import use_setuptools
-use_setuptools()
 
 from setuptools import setup, find_packages
 
@@ -11,12 +9,17 @@ setup(
     install_requires = [
         'django >= 1.3.1',
         #'fabric >= 1.0.0',
-        'virtualenv >= 1.4.5',
-        'celery',
-        'django-kombu',
-        'django-celery',
-        'jinja2',
+        'virtualenv >= 1.5.1',
+        #'celery',
+        #'django-kombu',
+        #'django-celery',
+        #'jinja2',
     ],
+    entry_points = {
+        'console_scripts': [
+            'cannula-admin = cannula.bin.admin:main',
+        ]
+    },
     url = 'http://bitbucket.org/rmyers/cannula/',
     description = ("A library of tools for deploying Python applications."),
     packages = find_packages(),
