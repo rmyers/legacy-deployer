be1dbeb66f266c41553dd6091b420f489d49329d
Robert Myers <robert@keyingredient.com>
2011-11-10 23:13:00 -0600

    Updating deploy and example app.yaml file

--- cannula/api/v2/deploy.py
+++ cannula/api/v2/deploy.py
@@ -15,15 +15,12 @@ log = getLogger('api')
 class Handler(object):
     """Simple object to hold properties for wsgi handlers"""
     
-    def __init__(self, url='', worker='', server={}, **kwargs):
-        self.url = url
-        self.worker = import_object(worker)
-        self.server = server
-        self.kwargs = kwargs
+    def __init__(self, name, worker, **kwargs):
+        self._worker_klass = import_object(worker)
+        self._worker_obj = self._worker_klass(name, **kwargs)
     
-    @property
-    def conf_template(self):
-        pass
+    def __getattr__(self, attr):
+        return getattr(self._worker_obj, attr)
     
 class DeployAPI(BaseAPI):
     
@@ -44,8 +41,31 @@ class DeployAPI(BaseAPI):
         #        # check against repo
         #        # cd project; git-log -1 -- app.yaml | awk '/commit/ {print $2}'
         #
+        
+        # Simple counter to make unique names for each handler
+        handler_position = 0
+        vhost_sections = []
         for handler in config.get('handlers', []):
-            
-            
-        worker = import_object(config['worker'])
-        write_file()        
+            if handler.get('worker'):
+                # Setup worker
+                name = '%s_%d' % (project.name, handler_position)
+                handle = Handler(name, **handler)
+                # write out start up scripts
+                handle.write_supervisor_conf(project)
+                handle.write_startup_script(project)
+                # add handler to vhost_sections
+                vhost_sections.append(handle)
+                handler_position += 1
+            else:
+                # Just pass the dictionary to the proxy vhosts
+                vhost_sections.append(handler)
+        
+        # Write out the proxy file to serve this app
+        ctx = {
+            'sections': vhost_sections,
+            'domain': config.get('domain', project.default_domain),
+            'runtime': config.get('runtime', 'python'),
+            'port': config.get('port', 80)
+        }
+        write_file(project.vhost_conf, proxy.template, ctx)
+                
