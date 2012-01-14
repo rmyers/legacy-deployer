917d15ca99d767cf4e71cfb7a185b186ac5344fb
Robert Myers <robert@keyingredient.com>
2012-01-14 14:38:20 -0600

    switching back to django

--- cannula/static/cannula/js/cannula.js
+++ cannula/static/cannula/js/cannula.js
@@ -39,6 +39,7 @@ var Group = function(obj) {
 var Project = function(obj) {
     jQuery.proxy(Model, this)(obj);
     this.group = obj.group
+    this.description = ko.observable(obj.description)
     this.absolute_url = '/' + obj.group + '/' + this.name() + '/';
     this.url = '/api/projects/' + this.name();
 }
@@ -64,6 +65,11 @@ var Collection = function() {
         this.items.remove(item);
     };
     
+    // Callback for new object creation form data
+    this.newOptions = function () {
+        return {};
+    };
+    
     // Extract data and enrich it with model object
     this.extractData = function(data) {
         if (data.objects) {
@@ -78,6 +84,34 @@ var Collection = function() {
             return data.objects;
         }
         return [];
+    };
+    
+    
+    this.addObject = function(formElement) {
+        this.working(true);
+
+        jQuery.ajax({
+            url : this.url,
+            type : 'POST',
+            data : this.newOptions(),
+            success : $.proxy(function(data) {
+                if(data.errorMsg) {
+                    this.errors(data.errorMsg);
+                } else {
+                    var obj = new this.model(data);
+                    obj.onDelete = jQuery.proxy(this.itemDeleted, this);
+                    this.items.push(obj);
+                    this.resetForm();
+                }
+            }, this),
+            error : $.proxy(function(data) {
+                console.log(data);
+                this.errors(data);
+            }, this),
+            complete : $.proxy(function() {
+                this.working(false);
+            }, this)
+        });
     };
     
     // Grab list of objects
@@ -111,6 +145,13 @@ var GroupCollection = function() {
     this.errors = ko.observable();
     this.formVisible = ko.observable(false);
     this.model = Group;
+    
+    this.newOptions = function () {
+        return {
+            name: this.newName(),
+            description: this.newDescription(),    
+        };
+    };
 
     this.resetForm = function() {
         this.newName('');
@@ -122,38 +163,57 @@ var GroupCollection = function() {
     this.toggleForm = function() {
         this.formVisible(!this.formVisible());
     };
+    
+    // Grab the initial groups
+    this.fetch();
+    
+};
 
-    this.addGroup = function(formElement) {
-        this.working(true);
+var ProjectCollection = function(groupName) {
+    jQuery.proxy(Collection, this)();
+    this.url = '/api/projects/';
+    this.options = {group: groupName};
+    this.newName = ko.observable('');
+    this.newDescription = ko.observable('');
+    this.errors = ko.observable();
+    this.formVisible = ko.observable(false);
+    this.model = Project;
+    
+    this.newOptions = function () {
+        return {
+            name: this.newName(),
+            description: this.newDescription(),
+            group: groupName,
+        };
+    };
+    
+    this.resetForm = function() {
+        this.newName('');
+        this.newDescription('');
+        this.formVisible(false);
+        this.errors('');
+    };
 
-        jQuery.ajax({
-            url : this.url,
-            type : 'POST',
-            data : {
-                name : this.newName(),
-                description : this.newDescription()
-            },
-            success : $.proxy(function(data) {
-                if(data.errorMsg) {
-                    this.errors(data.errorMsg);
-                } else {
-                    var group = new Group(data);
-                    group.onDelete = jQuery.proxy(this.itemDeleted, this);
-                    this.items.push(group);
-                    this.resetForm();
-                }
-            }, this),
-            error : $.proxy(function(data) {
-                console.log(data);
-                this.errors(data);
-            }, this),
-            complete : $.proxy(function() {
-                this.working(false);
-            }, this)
-        });
+    this.toggleForm = function() {
+        this.formVisible(!this.formVisible());
     };
     
     // Grab the initial groups
     this.fetch();
     
+};
+
+// Here's a custom Knockout binding that makes elements shown/hidden via jQuery's fadeIn()/fadeOut() methods
+// Could be stored in a separate utility library
+ko.bindingHandlers.fadeVisible = {
+    init: function(element, valueAccessor) {
+        // Initially set the element to be instantly visible/hidden depending on the value
+        var value = valueAccessor();
+        $(element).toggle(ko.utils.unwrapObservable(value)); // Use "unwrapObservable" so we can handle values that may or may not be observable
+    },
+    update: function(element, valueAccessor) {
+        // Whenever the value subsequently changes, slowly fade the element in or out
+        var value = valueAccessor();
+        ko.utils.unwrapObservable(value) ? $(element).fadeIn() : $(element).fadeOut();
+    }
 };
