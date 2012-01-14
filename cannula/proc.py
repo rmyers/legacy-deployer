a361b2f5e103f7fa9583af7f58b6c99103e7ecf3
Robert Myers <robert@keyingredient.com>
2012-01-14 15:40:40 -0600

    moving proc and proxy to api

--- cannula/proc.py
+++
@@ -1,5 +0,0 @@
-
-from cannula.conf import CANNULA_SUPERVISOR
-from cannula.utils import import_object
-
-supervisor = import_object(CANNULA_SUPERVISOR)
--- cannula/proxy.py
+++
@@ -1,5 +0,0 @@
-
-from cannula.conf import CANNULA_PROXY
-from cannula.utils import import_object
-
-proxy = import_object(CANNULA_PROXY)
