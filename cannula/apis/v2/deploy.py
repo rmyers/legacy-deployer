28f61410a8a9c165d00264563d23ff97049ec248
Robert Myers <robert@keyingredient.com>
2012-01-28 23:13:03 -0600

    Reworking deploy api to use new runtime for bootstrapping and git helper.

--- cannula/apis/v2/deploy.py
+++ cannula/apis/v2/deploy.py
@@ -13,7 +13,9 @@ from django.db.models.loading import get_model
 from cannula.apis import BaseAPI, ApiError
 from cannula.api import api
 from cannula import conf
-from cannula.utils import write_file, shell, import_object, Git
+from cannula.git import Git
+from cannula.utils import shell, import_object
+import datetime
 
 log = getLogger('api')
 
@@ -32,113 +34,125 @@ class DeployAPI(BaseAPI):
     
     model = get_model('cannula', 'deployment')
     
-    # Command for interacting with conf repos
-    git_add_cmd = '%s add --all' % conf.CANNULA_GIT_CMD
-    git_reset = '%s reset --hard' % conf.CANNULA_GIT_CMD
-    git_status = "%s status -s |awk '{print $2}'" % conf.CANNULA_GIT_CMD
-    git_log = "%s log -1 |awl '/commit/ {print $2}'" % conf.CANNULA_GIT_CMD
-    git_commit = '%s reset --hard' % conf.CANNULA_GIT_CMD
-    
     def _create(self, project, user, oldrev, newrev, conf_oldrev, conf_newrev):
         self.model.objects.create(project=project, user=user, oldrev=oldrev,
             newrev=newrev, conf_oldrev=conf_oldrev, conf_newrev=conf_newrev)
     
-    def deploy(self, project, user, oldrev=None, newrev=None):
+    def deploy(self, project, user, oldrev, newrev):
         user = api.users.get(user)
         project = api.projects.get(project)
         if not os.path.isfile(project.appconfig):
             raise ApiError("Project missing app.yaml file!")
         
-        if not os.path.isdir(project.conf_dir):
-            os.makedirs(project.conf_dir)
-            shell(Git.init, cwd=project.conf_dir)
-        
         with open(project.appconfig) as f:
-            # Attempt to get an exclusive lock on this file
+            # Attempt to get an exclusive lock on the app.yaml
+            # file. A way to ensure only one process can deploy
+            # at any single time. This is hard because deployment is
+            # triggered by a git push (which is just an ssh connection)
+            # TODO: Add test to verify this works/find reliable lock
             try:
                 fcntl.flock(f, fcntl.LOCK_EX|os.O_NDELAY)
             except IOError:
                 raise ApiError("Another person is deploying?")
             
-            # Copy the project to the conf_dir and add any new files
+            # Store the configuration for this project in git repo
+            # so that we can roll back to previous states
+            conf_dir = Git(project.conf_dir)
+            
+            if not os.path.isdir(project.conf_dir):
+                os.makedirs(project.conf_dir)
+                conf_dir.init()
+                # Add an initial commit, just to make a rollback point.
+                shell('touch app.yaml', cwd=project.conf_dir)
+                conf_dir.add_all()
+                conf_dir.commit("Initial Commit")
+            
+            # Copy the project app.yaml to the conf_dir
             shutil.copy(project.appconfig, project.deployconfig)
-            shell(self.git_add_cmd, cwd=project.conf_dir)
-            _, changed = shell(self.git_status, cwd=project.conf_dir)
             
-            if changed:
-                # we have changes in app.yaml lets reset everything
-                config = yaml.load(f.read())
+            # read in the application configuration
+            app = yaml.load(f.read())
             
-                # Simple counter to make unique names for each handler
-                handler_position = 0
-                sections = []
-                for handler in config.get('handlers', []):
-                    if handler.get('worker'):
-                        # Setup worker
-                        name = '%s_%d' % (project.name, handler_position)
-                        handle = Handler(name, project, **handler)
-                        # write out start up scripts
-                        handle.write_startup_script()
-                        # add handler to vhost_sections
-                        sections.append(handle)
-                        handler_position += 1
-                    else:
-                        # Just pass the dictionary to the proxy vhosts
-                        sections.append(handler)
+            # setup any runtime specific things here
+            try:
+                runtime = import_object(app.get('runtime'))
+            except ImportError:
+                raise ApiError("Unsupported runtime!")
+            # runtime bootstrap, setup project environment here
+            runtime.bootstrap(project, app)    
                 
-                # Write out the proxy file to serve this app
-                ctx = {
-                    'sections': sections,
-                    'domain': config.get('domain', 'localhost'),
-                    'runtime': config.get('runtime', 'python'),
-                    'port': config.get('port', 80),
-                    'project_conf_dir': project.conf_dir,
-                    'conf_dir': os.path.join(conf.CANNULA_BASE, 'config'),
-                    'project': project,
-                }
-                api.proxy.write_vhost_conf(project, ctx)
-                api.proc.write_project_conf(project, ctx)
-                
-                # Check if any files changed and check if still valid
-                shell(self.git_add_cmd, cwd=project.conf_dir)
-                _, changed = shell(self.git_status, cwd=project.conf_dir)
-                logging.debug(changed)
-                if re.search('vhost.conf', changed):
-                    try:
-                        api.proxy.restart()
-                    except:
-                        logging.exception("Error restarting proxy")
-                        shell(self.git_reset, cwd=project.conf_dir)
-                        raise ApiError("Deployment failed")
-                if re.search('supervisor.conf', changed):
-                    try:
-                        api.proc.reread()
-                    except:
-                        logging.exception("Error reading supervisor configs")
-                        shell(self.git_reset, cwd=project.conf_dir)
-                        raise ApiError("Deployment failed")
-                
-                # Add the project
-                api.proc.add_project(project.name)
-                api.proc.reread()
+        
+            # Simple counter to make unique names for each handler
+            # and keep them in order
+            handler_position = 0
+            sections = []
+            for handler in app.get('handlers', []):
+                if handler.get('worker'):
+                    # Setup worker
+                    name = '%s_%d' % (project.name, handler_position)
+                    handle = Handler(name, project, **handler)
+                    # write out bash start up scripts
+                    handle.write_startup_script()
+                    # add handler to vhost_sections
+                    sections.append(handle)
+                    handler_position += 1
+                else:
+                    # Just pass the dictionary to the proxy vhosts
+                    sections.append(handler)
+            
+            # Write out the proxy file to serve this app
+            ctx = {
+                'sections': sections,
+                'domain': app.get('domain', 'localhost'),
+                'runtime': app.get('runtime', 'python'),
+                'port': app.get('port', 80),
+                'project_conf_dir': project.conf_dir,
+                'conf_dir': os.path.join(conf.CANNULA_BASE, 'config'),
+                'project': project,
+            }
+            api.proxy.write_vhost_conf(project, ctx)
+            api.proc.write_project_conf(project, ctx)
+            
+            # Check if any files changed and check if still valid
+            conf_dir.add_all()
+            _, changed = conf_dir.status()
+            logging.debug(changed)
+            if re.search('vhost.conf', changed):
+                try:
+                    api.proxy.restart()
+                except:
+                    logging.exception("Error restarting proxy")
+                    conf_dir.reset()
+                    raise ApiError("Deployment failed")
+            if re.search('supervisor.conf', changed):
+                try:
+                    api.proc.reread()
+                except:
+                    logging.exception("Error reading supervisor configs")
+                    conf_dir.reset()
+                    raise ApiError("Deployment failed")
+            
+            # Add the project
+            api.proc.add_project(project.name)
+            api.proc.reread()
                 
             # Restart the project
             try:
                 api.proc.restart(project.name)
             except:
                 logging.exception("Error restarting project")
-                shell(self.git_reset, cwd=project.conf_dir)
+                conf_dir.reset()
                 raise ApiError("Deployment failed")
             # Current revision of conf directory
-            _, conf_oldrev = shell(self.git_log, cwd=project.conf_dir)
+            conf_oldrev = conf_dir.head()
             if changed:
                 # Commit config changes
-                shell(self.git_commit % user, cwd=project.conf_dir)
+                conf_dir.commit("Configuration: %s" % datetime.datetime.now().ctime())
             
             # new revision of conf directory
-            _, conf_newrev = shell(self.git_log, cwd=project.conf_dir)
-            logging.info("Old Project commit: %s", conf_oldrev)
-            logging.info("New Project commit: %s", conf_newrev)
+            conf_newrev = conf_dir.head()
+            if oldrev is None:
+                oldrev = "Initial Commit"
             self._create(project, user, oldrev, newrev, conf_oldrev, conf_newrev)
             # finally release the lock
             fcntl.flock(f, fcntl.LOCK_UN)       
