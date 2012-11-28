cfe58cb8dbeef90d425ce1199cd6795b45eda5f9
Robert Myers <robert.myers@rackspace.com>
2012-11-28 09:25:40 -0600

    Adding deploy locking object

--- cannula/apis/v2/deploy.py
+++ cannula/apis/v2/deploy.py
@@ -28,7 +28,42 @@ class Handler(object):
     
     def __getattr__(self, attr):
         return getattr(self._worker_obj, attr)
+
+class DeployLock(object):
+    """Lock a deployment so that only one deploy happens at a time.
     
+    This is meant to be used in a context like this::
+    
+        user = api.users.get(user)
+        project = api.projects.get(project)
+        with DeployLock(project, user):
+            do_deploy()
+    """
+    
+    def __init__(self, project, user):
+        self.project = project
+        self.user = user
+        self.locked = False
+        self.lock_file = os.path.join(project.conf_dir, '__locked__')
+    
+    def __enter__(self):
+        if os.path.isfile(self.lock_file):
+            # the project is locked
+            with open(self.lock_file) as l:
+                lock_info = l.read()
+            raise Exception("Project locked by: %s" % lock_info)
+        with open(self.lock_file, 'a') as l:
+            l.write('%s - %s' % (self.user, datetime.datetime.now()))
+            self.locked = True
+    
+    def __exit__(self, ex, value, trace):
+        if value is not None:
+            raise
+        if self.locked:
+            os.remove(self.lock_file)
+        
+    
+
 class DeployAPI(BaseAPI):
     
     model = get_model('cannula', 'deployment')
@@ -43,123 +78,117 @@ class DeployAPI(BaseAPI):
         if not os.path.isfile(project.appconfig):
             raise ApiError("Project missing app.yaml file!")
         
-        with open(project.appconfig) as f:
-            # Attempt to get an exclusive lock on the app.yaml
-            # file. A way to ensure only one process can deploy
-            # at any single time. This is hard because deployment is
-            # triggered by a git push (which is just an ssh connection)
-            # TODO: Add test to verify this works/find reliable lock
-            try:
-                fcntl.flock(f, fcntl.LOCK_EX|os.O_NDELAY)
-            except IOError:
-                raise ApiError("Another person is deploying?")
+        # Attempt to get an exclusive lock on the project
+        # file. A way to ensure only one process can deploy
+        # at any single time. This is hard because deployment is
+        # triggered by a git push (which is just an ssh connection)
+        with DeployLock(project, user):
+            with open(project.appconfig) as f:
+                
+                # Store the configuration for this project in git repo
+                # so that we can roll back to previous states
+                conf_dir = Git(project.conf_dir)
+                
+                if not os.path.isdir(project.conf_dir):
+                    os.makedirs(project.conf_dir)
+                    conf_dir.init()
+                    # Add an initial commit, just to make a rollback point.
+                    open(project.deployconfig, 'a')
+                    conf_dir.add_all()
+                    conf_dir.commit("Initial Commit")
+                
+                # Copy the project app.yaml to the conf_dir
+                shutil.copy(project.appconfig, project.deployconfig)
+                
+                # read in the application configuration
+                app = yaml.load(f.read())
+                
+                # setup any runtime specific things here
+                try:
+                    runtime = import_object(app.get('runtime'))
+                except ImportError:
+                    raise ApiError("Unsupported runtime!")
+                # runtime bootstrap, setup project environment here
+                runtime.bootstrap(project, app)    
+                    
             
-            # Store the configuration for this project in git repo
-            # so that we can roll back to previous states
-            conf_dir = Git(project.conf_dir)
-            
-            if not os.path.isdir(project.conf_dir):
-                os.makedirs(project.conf_dir)
-                conf_dir.init()
-                # Add an initial commit, just to make a rollback point.
-                open(project.deployconfig, 'a')
+                # Simple counter to make unique names for each handler
+                # and keep them in order
+                handler_position = 0
+                sections = []
+                for handler in app.get('handlers', []):
+                    if handler.get('worker'):
+                        # Setup worker
+                        name = '%s_%d' % (project.name, handler_position)
+                        # defaults are special, they reference another
+                        # section in the app.yaml
+                        defaults = handler.pop('defaults', None)
+                        if defaults:
+                            handler_defaults = app.get(defaults, {})
+                            handler.update(handler_defaults)
+                        handle = Handler(name, project, **handler)
+                        # write out bash start up scripts
+                        handle.write_startup_script()
+                        # add handler to vhost_sections
+                        sections.append(handle)
+                        handler_position += 1
+                    else:
+                        # Just pass the dictionary to the proxy vhosts
+                        sections.append(handler)
+                
+                # Write out the proxy file to serve this app
+                ctx = {
+                    'sections': sections,
+                    'domain': app.get('domain', 'localhost'),
+                    'runtime': app.get('runtime', 'python'),
+                    'port': app.get('port', 80),
+                    'project_conf_dir': project.conf_dir,
+                    'conf_dir': os.path.join(conf.CANNULA_BASE, 'config'),
+                    'project': project,
+                }
+                api.proxy.write_vhost_conf(project, ctx)
+                api.proc.write_project_conf(project, ctx)
+                
+                # Check if any files changed and check if still valid
                 conf_dir.add_all()
-                conf_dir.commit("Initial Commit")
-            
-            # Copy the project app.yaml to the conf_dir
-            shutil.copy(project.appconfig, project.deployconfig)
-            
-            # read in the application configuration
-            app = yaml.load(f.read())
-            
-            # setup any runtime specific things here
-            try:
-                runtime = import_object(app.get('runtime'))
-            except ImportError:
-                raise ApiError("Unsupported runtime!")
-            # runtime bootstrap, setup project environment here
-            runtime.bootstrap(project, app)    
+                _, changed = conf_dir.status()
+                logging.debug(changed)
+                if re.search('vhost.conf', changed):
+                    # Vhost file is either new or changed which will require 
+                    # our proxy server to reload its configuration files.
+                    try:
+                        api.proxy.restart()
+                    except:
+                        logging.exception("Error restarting proxy")
+                        conf_dir.reset()
+                        raise ApiError("Deployment failed")
+                if re.search('supervisor.conf', changed):
+                    try:
+                        api.proc.reread()
+                    except:
+                        logging.exception("Error reading supervisor configs")
+                        conf_dir.reset()
+                        raise ApiError("Deployment failed")
                 
-        
-            # Simple counter to make unique names for each handler
-            # and keep them in order
-            handler_position = 0
-            sections = []
-            for handler in app.get('handlers', []):
-                if handler.get('worker'):
-                    # Setup worker
-                    name = '%s_%d' % (project.name, handler_position)
-                    # defaults are special, they reference another
-                    # section in the app.yaml
-                    defaults = handler.pop('defaults', None)
-                    if defaults:
-                        handler_defaults = app.get(defaults, {})
-                        handler.update(handler_defaults)
-                    handle = Handler(name, project, **handler)
-                    # write out bash start up scripts
-                    handle.write_startup_script()
-                    # add handler to vhost_sections
-                    sections.append(handle)
-                    handler_position += 1
-                else:
-                    # Just pass the dictionary to the proxy vhosts
-                    sections.append(handler)
-            
-            # Write out the proxy file to serve this app
-            ctx = {
-                'sections': sections,
-                'domain': app.get('domain', 'localhost'),
-                'runtime': app.get('runtime', 'python'),
-                'port': app.get('port', 80),
-                'project_conf_dir': project.conf_dir,
-                'conf_dir': os.path.join(conf.CANNULA_BASE, 'config'),
-                'project': project,
-            }
-            api.proxy.write_vhost_conf(project, ctx)
-            api.proc.write_project_conf(project, ctx)
-            
-            # Check if any files changed and check if still valid
-            conf_dir.add_all()
-            _, changed = conf_dir.status()
-            logging.debug(changed)
-            if re.search('vhost.conf', changed):
-                # Vhost file is either new or changed which will require 
-                # our proxy server to reload its configuration files.
+                # Add the project
+                api.proc.reread(stderr=True)
+                api.proc.add_project(project.name)
+                    
+                # Restart the project
                 try:
-                    api.proxy.restart()
+                    api.proc.restart(project.name, stderr=True)
                 except:
-                    logging.exception("Error restarting proxy")
+                    logging.exception("Error restarting project")
                     conf_dir.reset()
                     raise ApiError("Deployment failed")
-            if re.search('supervisor.conf', changed):
-                try:
-                    api.proc.reread()
-                except:
-                    logging.exception("Error reading supervisor configs")
-                    conf_dir.reset()
-                    raise ApiError("Deployment failed")
-            
-            # Add the project
-            api.proc.reread(stderr=True)
-            api.proc.add_project(project.name)
+                # Current revision of conf directory
+                conf_oldrev = conf_dir.head()
+                if changed:
+                    # Commit config changes
+                    conf_dir.commit("Configuration: %s" % datetime.datetime.now().ctime())
                 
-            # Restart the project
-            try:
-                api.proc.restart(project.name, stderr=True)
-            except:
-                logging.exception("Error restarting project")
-                conf_dir.reset()
-                raise ApiError("Deployment failed")
-            # Current revision of conf directory
-            conf_oldrev = conf_dir.head()
-            if changed:
-                # Commit config changes
-                conf_dir.commit("Configuration: %s" % datetime.datetime.now().ctime())
-            
-            # new revision of conf directory
-            conf_newrev = conf_dir.head()
-            if oldrev is None:
-                oldrev = "Initial Commit"
-            self._create(project, user, oldrev, newrev, conf_oldrev, conf_newrev)
-            # finally release the lock
-            fcntl.flock(f, fcntl.LOCK_UN)       
+                # new revision of conf directory
+                conf_newrev = conf_dir.head()
+                if oldrev is None:
+                    oldrev = "Initial Commit"
+                self._create(project, user, oldrev, newrev, conf_oldrev, conf_newrev)     
