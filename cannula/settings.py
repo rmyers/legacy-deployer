0ed1a9d513e788d24a73dd116bd6075fc7e70fbe
Robert Myers <robert.myers@rackspace.com>
2012-11-27 21:53:32 -0600

    Fixing static files

--- cannula/settings.py
+++ cannula/settings.py
@@ -48,13 +48,22 @@ USE_L10N = config.getboolean('django', 'use_l10n')
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/"
-
 MEDIA_ROOT = os.path.join(CANNULA_MODULE_DIR, 'static')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = config.get('django', 'media_url')
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(CANNULA_MODULE_DIR, 'static_root')
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
