0df4975ad531fb302dda44fcfe4bb9559963e53a
Robert Myers <robert.myers@rackspace.com>
2013-02-16 21:00:37 -0600

    Updating styles a little bit for the signup/signin pages.

--- assets/js/src/app.js
+++ assets/js/src/app.js
@@ -4,23 +4,13 @@
 // Declare app level module which depends on filters, and services
 var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ngCookies','http-auth-interceptor']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: '/static/partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: '/static/partials/partial2.html', controller: MyCtrl2});
+    $routeProvider.when('/dashboard', {templateUrl: '/static/partials/partial1.html', controller: MyCtrl1});
+    $routeProvider.when('/signup', {templateUrl: '/static/partials/signup.html', controller: SignupController});
     $routeProvider.when('/people', {templateUrl: '/static/partials/people.html', controller: PeopleController});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.otherwise({redirectTo: '/dashboard'});
   }]);
 
 // Turn on html5 mode
 myApp.config(['$locationProvider', function($locationProvider){
     $locationProvider.html5Mode(true).hashPrefix('!');	
-}]);
-
-// Make a reference to httpProvider for use later
-myApp.config(['$httpProvider', function($httpProvider) {
-  myApp.$httpProvider = $httpProvider;
-}]);
-
-// Add a default header for the Django csrftoken
-myApp.run(['$cookies', function($cookies){
-  myApp.$httpProvider.defaults.headers.post['X-CSRFToken'] = $cookies['csrftoken'];
 }]);
--- assets/js/src/controllers.js
+++ assets/js/src/controllers.js
@@ -2,14 +2,37 @@
 
 /* Controllers */
 
+function MainCntl($scope, $route, $routeParams, $location) {
+  $scope.$route = $route;
+  $scope.$location = $location;
+  $scope.$routeParams = $routeParams;
+};
 
 function MyCtrl1() {}
 MyCtrl1.$inject = [];
 
 
-function MyCtrl2() {
-}
-MyCtrl2.$inject = [];
+function SignupController($scope, $http, authService) {
+  $scope.page = 'signup';
+  $scope.submit = function () {
+  	if ($scope.password != $scope.confirm) {
+  	  $scope.error = "Passwords do not match!";
+  	} else {
+		$http.post('/accounts/signup/', jQuery.param($scope.user)).
+	      success(function() {
+			authService.loginConfirmed();
+			$scope.username = '';
+			$scope.password = '';
+			$scope.error = false;
+		  }).
+		  error(function(resp) {
+		  	console.log(resp);
+			$scope.error = resp.message;
+			authService.loginConfirmed();
+		  });
+	    };
+  	}
+};
 
 function PeopleController($resource, $scope, People) {
     People.query({}, function(data){
@@ -20,7 +43,7 @@ function PeopleController($resource, $scope, People) {
 
 function LoginController($scope, $http, authService) {
 	$scope.submit = function () {
-		$http.post('../accounts/login/', 
+		$http.post('/accounts/login/', 
 		    'username='+$scope.username+'&password='+$scope.password).
 		    success(function() {
 				authService.loginConfirmed();
@@ -34,5 +57,6 @@ function LoginController($scope, $http, authService) {
 	};
 };
 
+SignupController.$inject = ['$scope', '$http', 'authService']
 PeopleController.$inject = ['$resource', '$scope', 'People'];
 LoginController.$inject = ['$scope', '$http', 'authService']
